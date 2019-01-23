203aaa1c3dd9aa9f67dc518e817161b9bf7c59bc
Matt Migliore <mmigliore311@gmail.com>
2019-01-23 10:51:34 -0500

    init

--- src/containers/PlannedTrips.js
+++ src/containers/PlannedTrips.js
@@ -3,27 +3,27 @@ import TripPreview from '../components/TripPreview'
 import TripDetails from  '../components/TripDetails'
 
 class PlannedTrips extends Component {
-  state = {
-    tripId: null
-  }
-
-  setSelectedTrip = (tripId) => {
-    console.log(tripId);
-    this.setState({
-      tripId: tripId
-    })
-  }
+  // state = {
+  //   tripId: null
+  // }
+  //
+  // setSelectedTrip = (tripId) => {
+  //   console.log(tripId);
+  //   this.setState({
+  //     tripId: tripId
+  //   })
+  // }
 
 
   render() {
-    const trips = this.props.trips.map(trip => {
-      return <TripPreview key={trip.trip.id} trip={trip} setSelectedTrip={this.setSelectedTrip}/>
-    })
+
 
     return (
       <div className="PlannedTrips col-md-6">
         <h2>Planned Trips</h2>
-        {this.state.tripId ? <TripDetails currentUserId={this.props.currentUserId} tripId={this.state.tripId} editTrip={this.props.editTrip} deleteTrip={this.props.deleteTrip}/> : trips}
+        {this.props.trips.map(trip => {
+          return <TripPreview key={trip.trip.id} trip={trip} setSelectedTrip={this.setSelectedTrip}/>
+        })}
       </div>
     );
   }
--- src/containers/TripList.js
+++ src/containers/TripList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import PlannedTrips from './PlannedTrips'
 import CompletedTrips from './CompletedTrips'
+import TripDetails from  '../components/TripDetails'
 
 class TripList extends Component {
 
   state = {
-    trips: []
+    trips: [],
+    tripId: null
+  }
+
+  setSelectedTrip = (tripId) => {
+    console.log(tripId);
+    this.setState({
+      tripId: tripId
+    })
   }
 
   componentDidMount() {
@@ -18,13 +27,22 @@ class TripList extends Component {
     })
   }
 
+
+
+
   render() {
+    let detailBox
+    if (this.state.tripId) {
+      detailBox = <TripDetails currentUserId={this.props.currentUserId} tripId={this.state.tripId} editTrip={this.props.editTrip} deleteTrip={this.props.deleteTrip}/>
+    }
+
     return (
       <div className="TripList container">
         <div className="row">
-          <PlannedTrips currentUserId={this.props.currentUserId} trips={this.state.trips.filter(trip => !trip.trip.completed)} editTrip={this.props.editTrip} deleteTrip={this.props.deleteTrip}/>
+          <PlannedTrips trips={this.state.trips.filter(trip => !trip.trip.completed)}/>
           <CompletedTrips trips={this.state.trips.filter(trip => trip.trip.completed)}/>
         </div>
+
       </div>
     );
   }
