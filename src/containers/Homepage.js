bb1a738b3c76c444e5d0e0793b069184572caf57
Eric Laitman <elaitman1@gmail.com>
2019-01-23 14:40:30 -0500

    wednesday 2:40pm

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,14 +1,89 @@
 import React, {Component} from 'react'
 import { Link } from "react-router-dom"
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 
 class Homepage extends Component {
+
+  state = {
+    signUp: false,
+    name: '',
+    username: '',
+    password: '',
+    homecity:''
+
+  }
+
+  signUpButtonClick = () => {
+      this.setState({
+        signUp: !this.state.signUp,
+      })
+    }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.setState({
+        name: e.target.name,
+        username: e.target.username,
+        password: e.target.password,
+        home_city: e.target.homecity
+    })
+
+    fetch(`http://localhost:4000/api/v1/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        home_city: this.state.homecity,
+        password_digest: this.state.password,
+        username: this.state.username,
+        home_city: this.state.homecity
+      })
+    })
+    // .then(window.location.href = '/trip-list')
+  }
+
+
+
   render() {
     return (
       <div>
-        <h2>Homepage Placeholder</h2>
-        <Link to="/new-trip"><button>New Trip</button></Link>
-        <Link to="/trip-list"><button>Trip List</button></Link>
-      </div>
+      {this.state.signUp ?
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Name</label>
+            <input name={this.state.name} placeholder='name' />
+          </Form.Field>
+          <Form.Field>
+              <label>UserName</label>
+              <input username={this.state.username} placeholder='userName' />
+          </Form.Field>
+          <Form.Field>
+              <label>Password</label>
+              <input password={this.state.password} placeholder='password' />
+          </Form.Field>
+          <Form.Field>
+              <label>Home City</label>
+              <input homecity={this.state.homecity} placeholder='homecity' />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
+        </Form> :
+        <Form>
+          <Form.Field>
+            <label>UserName</label>
+            <input placeholder='userName' />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input placeholder='password' />
+          </Form.Field>
+          <Button type='submit'>Login</Button>
+          <Button onClick={this.signUpButtonClick} type='submit'>! Sign Up !</Button>
+        </Form>
+    }
+    </div>
     )
   }
 }
